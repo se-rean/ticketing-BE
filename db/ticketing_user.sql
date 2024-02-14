a7a27828c081e4d7da8c289d3b66c848933c1f17
Rbandoy <reneboy.andoy@e-horizon.com>
2024-02-15 04:09:32 +0800

    update db

--- db/ticketing_user.sql
+++ db/ticketing_user.sql
@@ -23,8 +23,8 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `createdAt` bigint DEFAULT NULL,
-  `updatedAt` bigint DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
