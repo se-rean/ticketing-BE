07c745d349f8aad3e5ede9f93ad39e2c67996723
Rbandoy <reneboy.andoy@e-horizon.com>
2024-02-22 13:48:49 +0800

    add pagination

--- controller/LogsController.js
+++ controller/LogsController.js
@@ -1,11 +1,26 @@
 const { apiResponse } = require("../api-helpers/ResponseController")
 const dataToSnakeCase = require("../api-helpers/data_to_snake_case")
 const LogsModel = require("../model/LoggerModel")
+const queryPaginate = require("../api-helpers/query-paginate");
 const LogsController = {}
 
 LogsController.get = async (req, res) => {
+  const { page, page_size, type } = req.query;
   try { 
-    const logs = await LogsModel.findAll({raw: true})
+    let query = {}
+    if (type) {
+      query.where = { type }
+    }
+    const logs = await LogsModel.findAll(
+      queryPaginate(
+        {
+          ...query,
+          order: [["type", "ASC"], ["createdAt", "DESC"]],
+          raw: true,
+        },
+        { page: page || 1, page_size: page_size || 3000 }
+      )
+    )
 
     res.send(dataToSnakeCase(apiResponse({
       statusCode: 200,
