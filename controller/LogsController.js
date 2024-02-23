664acd0a5a2559a60a78e551ecb3241e0ca6052b
Rbandoy <reneboy.andoy@e-horizon.com>
2024-02-23 10:00:49 +0800

    update get logs

--- controller/LogsController.js
+++ controller/LogsController.js
@@ -1,17 +1,36 @@
 const { apiResponse } = require("../api-helpers/ResponseController")
 const dataToSnakeCase = require("../api-helpers/data_to_snake_case")
 const LogsModel = require("../model/LoggerModel")
+const { UserModel } = require('../init/mysql-init')
 const queryPaginate = require("../api-helpers/query-paginate");
 const LogsController = {}
 
 LogsController.get = async (req, res) => {
   const { page, page_size, type } = req.query;
+  const { user } = req.user;
   try { 
-    let query = {}
-    if (type) {
-      query.where = { type }
+    let query = { where: {}}
+
+    const role = await UserModel.findAll({
+      where: {
+        id: user.id,
+        role: 'admin'
+      },
+        raw: true,
+        attributes: { exclude: ["password"] },
+    });
+    console.log(role.length)
+    if (role.length == 0) {
+      query.where.userId = user.id
     }
-    const logs = await LogsModel.findAll(
+
+    console.log(query)
+
+    if (type) {
+      query.where.type = type
+    }
+
+    const logs = await LogsModel.findAndCountAll(
       queryPaginate(
         {
           ...query,
